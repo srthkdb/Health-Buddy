a18221f50855bc4fb33016478fa870fc1f63e2c8
PrsMittal <parasm@iitk.ac.in>
2019-07-07 04:08:27 +0530

    Appointments updated

--- HealthBuddy/Doctor/urls.py
+++ HealthBuddy/Doctor/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('pres/<int:patient_roll>/<int:pres_id>/', views.add_med, name="change_prescription"),
     path('pres/save/<int:patient_roll>/<int:pres_id>/', views.save_pres, name="save_pres"),
     path('pres/save/<int:patient_roll>/', views.save_pres, name="save_pres_new"),
-    path('pres/<int:patient_roll>/<int:pres_id>/delete_med/<int:med_id>', views.delete_med, name="delete_med")
+    path('pres/<int:patient_roll>/<int:pres_id>/delete_med/<int:med_id>', views.delete_med, name="delete_med"),
+    path('pres/save/<int:patient_roll>/<int:pres_id>/<int:end>/', views.save_pres, name="save_and_end"),
+    path('pres/refer/<int:patient_roll>/<int:pres_id>/', views.create_pres_return, name="refer"),
+    path('pres/refer/<int:patient_roll>/', views.create_pres_return, name="refer_new"),
+    path('pres/create_reference/<int:patient_roll>/<int:pres_id>/', views.create_reference, name="create_reference"),
 ]
--- HealthBuddy/Doctor/views.py
+++ HealthBuddy/Doctor/views.py
@@ -6,13 +6,50 @@ from seven.models import TestList
 import datetime
 from Reception.models import Appointment
 
-
-def save_pres(request, patient_roll, pres_id=None, app_id, end=None):
+#saves pres and renders reference form
+def create_pres_return(request, patient_roll, pres_id=None):
     form_class_pres = PrescriptionForm
     form_class_med = PresMedicineForm
     template_name = 'Doctor/prescription_form.html'
     patient = get_object_or_404(Patient, pk=patient_roll)
-    app = get_object_or_404(Appointment, pk=app_id)
+    app = Appointment.objects.filter(patient=patient)
+    form_class = CreateReferForm
+    form = form_class(None)
+
+    if pres_id == None:
+        pres_form = form_class_pres(request.POST or None)
+        med_form = form_class_med(request.POST or None)
+    else:
+        pres = get_object_or_404(Prescription, pk=pres_id)
+        pres_form = form_class_pres(request.POST or None, instance=pres)
+        med_form = form_class_med(request.POST or None)
+
+    if pres_form.is_valid():
+        if pres_id == None:
+            current_time = datetime.datetime.now()
+            p = pres_form.save(commit=False)
+            p.date = current_time
+            p.doctor = request.user.doctor
+            p.patient = patient
+            p.med_added = True
+            p.save()
+
+            return render(request, 'Doctor/reference_form.html',{'form': form})
+        else:
+            p = pres_form.save(commit=False)
+            p.save()
+
+            return render(request, 'Doctor/reference_form.html',{'form': form})
+    return render(request, template_name,{'pres_form': pres_form, 'med_form': med_form, 'patient': patient, 'error_message': 'Error: Invalid form submission, cannot create reference'})
+
+
+#delete appointment by filtering from patient and not by using app_id
+def save_pres(request, patient_roll, pres_id=None, end=None):
+    form_class_pres = PrescriptionForm
+    form_class_med = PresMedicineForm
+    template_name = 'Doctor/prescription_form.html'
+    patient = get_object_or_404(Patient, pk=patient_roll)
+    app = Appointment.objects.filter(patient=patient)
 
     if pres_id == None:
         pres_form = form_class_pres(request.POST or None)
@@ -42,6 +79,7 @@ def save_pres(request, patient_roll, pres_id=None, app_id, end=None):
         else:
             p = pres_form.save(commit=False)
             p.save()
+
 
             med_form = form_class_med(None)
             pres_form = form_class_pres(None)
@@ -116,18 +154,19 @@ def add_med(request, patient_roll, pres_id=None):
 
     return render(request, template_name, {'pres_form': pres_form, 'med_form': med_form, 'patient': patient})
 
-def ReferAppointment(request, patient_roll, pres_id):
+def create_reference(request, patient_roll, pres_id=None):
+    form_class = CreateReferForm
+    template_name = 'Doctor/prescription_form.html'
+    form = form_class(request.POST or None)
+    doctor = request.user.doctor
     patient = get_object_or_404(Patient, pk=patient_roll)
-    prev_doc = request.user.doctor
-    pres = get_object_or_404(Prescription, pk=pred_id)
-    brief_str = 'Dr ' + prev_doc.user.first_name + ' ' + prev_doc.user.last_name + "'s' remarks: " + pres.refer_remarks
-    new_app = Appointment(
-        patient = patient,
-        dateNtime = datetime.datetime.now(),
-        brief = brief_str,
-        doctor = prev_app.doc_ref,
-        is_referred = True,
-    )
-    new_app.save()
+    if pres_id:
+        pres = create_pres_return(request, patient_roll, pres_id)
+    else:
+        pres = create_pres_return(request, patient_roll, pres_id=None)
 
-    return render()
+    if form.is_valid():
+        r = form.save(commit=False)
+        r.from_doc = doctor
+        r.prescription = pres
+        r.save()
