7511572ee24f81d8898dc6e64c358d9c09f34919
PrsMittal <parasm@iitk.ac.in>
2019-07-10 04:58:15 +0530

    Update Endpoints for Doctor, Reception, Patient

--- HealthBuddy/Doctor/urls.py
+++ HealthBuddy/Doctor/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('pres/save/<int:patient_roll>/', views.save_pres, name="save_pres_new"),
     path('pres/<int:patient_roll>/<int:pres_id>/delete_med/<int:med_id>', views.delete_med, name="delete_med"),
     path('pres/save/<int:patient_roll>/<int:pres_id>/<int:end>/', views.save_pres, name="save_and_end"),
+    path('pres/save/<int:patient_roll>/<int:end>/', views.save_pres, name="create_and_end"),
     path('pres/refer/<int:patient_roll>/<int:pres_id>/', views.redirect_ref_form, name="refer"),
     path('pres/refer/<int:patient_roll>/', views.redirect_ref_form, name="refer_new"),
     path('create_reference/<int:pres_id>/', views.create_reference, name="create_reference"),
